e35acd74b70773972414288d66e48e92475c05b2
Lionel Henry <lionel.hry@gmail.com>
2023-03-08 11:45:46 +0100

    Update Makevars

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,4 @@
-VERSION <- "2.7.4"
+VERSION <- commandArgs(TRUE)
 if(!file.exists(sprintf("../windows/harfbuzz-%s/include/png.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
   download.file(sprintf("https://github.com/rwinlib/harfbuzz/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
